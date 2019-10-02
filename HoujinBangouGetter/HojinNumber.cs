bf9c550e62cc0b1e42a8b724377a85d2f4b12379
Makoto Uwaya <makoto.uwaya@gmail.com>
2019-10-03 00:01:02 +0900

    Initialize repository

---
+++ HoujinBangouGetter/HojinNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HoujinBangouGetter
+{
+    public class HojinNumber
+    {
+        public string 法人番号 { get; }
+        public string 商号又は名称 { get; }
+        public string 所在地 { get; }
+        public Uri 変更履歴情報等 { get; }
+
+        public HojinNumber(string hojinNumber, string name, string address, string historyUrl)
+        {
+            this.法人番号 = hojinNumber;
+            this.商号又は名称 = name;
+            this.所在地 = address;
+            if (Uri.TryCreate(historyUrl, UriKind.Absolute, out var url))
+            {
+                this.変更履歴情報等 = url;
+            }
+        }
+
+        public static async Task<List<HojinNumber>> Search(string[] campanyNames)
+        {
+            var hojinNumbers = new List<HojinNumber>();
+
+
+            return hojinNumbers;
+        }
+    }
+}
---
+++ HoujinBangouGetter/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HoujinBangouGetter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OnClickSearchButton(object sender, RoutedEventArgs e)
+        {
+            var searchButton = sender as Button;
+            searchButton.IsEnabled = false;
+
+            try
+            {
+                var result = await HojinNumber.Search(this.SearchHojinNameTextBox.Text.Split('\n'));
+                this.ResultDataGrid.ItemsSource = result;
+            }
+            catch (Exception)
+            {
+                this.ResultDataGrid.ItemsSource = null;
+            }
+            finally
+            {
+                searchButton.IsEnabled = true;
+            }
+        }
+    }
+}
